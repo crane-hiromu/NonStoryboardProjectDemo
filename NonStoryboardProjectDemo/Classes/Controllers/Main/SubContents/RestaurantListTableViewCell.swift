0cff099f35a80499b337c763a3929ae402a18c91
T.H <h.turuta@leihauoli.com>
2017-07-16 00:55:24 +0900

    storyboardでリスト表示を設定

---
+++ NonStoryboardProjectDemo/Classes/Controllers/Main/SubContents/RestaurantListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RestaurantListTableViewCell.swift
+//  NonStoryboardProjectDemo
+//
+//  Created by T.H on 2017/07/15.
+//  Copyright © 2017年 T.H. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ NonStoryboardProjectDemo/Classes/Models/Extensions/UIView+Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  UIView+Extensions.swift
+//  NonStoryboardProjectDemo
+//
+//  Created by T.H on 2017/07/15.
+//  Copyright © 2017年 T.H. All rights reserved.
+//
+
+import Foundation
